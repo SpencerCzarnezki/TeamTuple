81139f2ca80707acc346eeb2c0241f52235b3f27
Nic Stuiber <nstuiber@gmail.com>
2021-11-01 15:17:25 -0500

    Resources and user event models

---
+++ event-api/src/main/java/Resources.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+public class Resources {
+    private String resource;
+    private int locationId;
+
+    public Resources() {
+    }
+
+    public Resources(String resource, int locationId) {
+        this.resource = resource;
+        this.locationId = locationId;
+    }
+
+    public String getResource() {
+        return resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resources resources = (Resources) o;
+        return locationId == resources.locationId && Objects.equals(resource, resources.resource);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(resource, locationId);
+    }
+}
---
+++ event-api/src/main/java/UserEvent.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+public class UserEvent {
+    private int userId;
+    private int eventId;
+
+    public UserEvent() {
+    }
+
+    public UserEvent(int userId, int eventId) {
+        this.userId = userId;
+        this.eventId = eventId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserEvent userEvent = (UserEvent) o;
+        return userId == userEvent.userId && eventId == userEvent.eventId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, eventId);
+    }
+}
