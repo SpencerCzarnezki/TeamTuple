4f25157d1254058fe9934292de66465198d4ea76
SpencerCzarnezki <czarnez5@gmail.com>
2021-11-01 14:59:13 -0500

    updated schema final monday

--- meetup-schema.sql
+++ meetup-schema.sql
@@ -2,14 +2,6 @@ drop database if exists meetup;
 create database if not exists meetup;
 use meetup;
 
-drop table if exists app_user_role;
-drop table if exists `role`;
-drop table if exists `user`;
-drop table if exists app_user_event;
-drop table if exists `event`;
-drop table if exists location;
-drop table if exists resources;
-
 CREATE TABLE location (
     locationId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(100),
@@ -77,15 +69,3 @@ CREATE TABLE app_user_role (
     CONSTRAINT fk_app_user_role_role_id FOREIGN KEY (app_role_id)
         REFERENCES `role` (roleId)
 );
-
-insert into `role` (title) values 
-    ('USER'),
-    ('ADMIN');
-
-insert into `user` (username, password_hash) values
-    ('user', '$2a$10$ZMZEHR/CeSDPh7o0dIUPJeSh6r2TGDMUuBSC.Vff6VGL2CGNUdB5q'),
-    ('admin', '$2a$10$L4c8Ky5D5O7WsVm89lTiPO3/pPtME0itveW5GuSI1.vVWSBS1bCJ2');
-
-insert into app_user_role (app_user_id, app_role_id) values
-	(1, 1),
-    (2, 2);
