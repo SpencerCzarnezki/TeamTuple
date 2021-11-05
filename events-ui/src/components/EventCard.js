d7e932c1cafa41ea27852a94be9c1b97f8955bc3
Nic Stuiber <nstuiber@gmail.com>
2021-11-05 16:00:52 -0500

    Added HTTP Requests and tweaked controllers

--- events-ui/src/components/EventCard.js
+++ events-ui/src/components/EventCard.js
@@ -1,19 +1,20 @@
-import { DocumentCard, DocumentCardDetails, DocumentCardLocation, DocumentCardTitle } from "@fluentui/react";
-import { Link } from "react-router-dom";
+import { DocumentCard, DocumentCardDetails, DocumentCardLocation, DocumentCardStatus, DocumentCardTitle } from "@fluentui/react";
+import { Link, useNavigate } from "react-router-dom";
 
 
 function EventCard({event}, {location}){
-        
+        // const navigate = useNavigate();
     function onClick(){
-        // history.push("/event/:eventId");
+        // navigate("/");
     }
     
-    
-
+        
     return(
        <DocumentCard onClick={onClick}>
-           <DocumentCardTitle>{event.title}</DocumentCardTitle>
-           <DocumentCardDetails>{event.description}</DocumentCardDetails>
+           {/* <DocumentCardTitle>{event.title}</DocumentCardTitle>
+           <DocumentCardDetails>{event.description}</DocumentCardDetails> */}
+           <DocumentCardTitle title="Title"></DocumentCardTitle>
+           <input type="datetime-local"></input>
        </DocumentCard>
 
     
