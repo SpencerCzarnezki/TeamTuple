df58512afc292d1eef7fda9d49f10ed25a68c860
SpencerCzarnezki <czarnez5@gmail.com>
2021-11-11 19:14:41 -0600

    updated schema; readied back end for AWS

--- event-api/src/main/java/learn/events/controllers/LocationController.java
+++ event-api/src/main/java/learn/events/controllers/LocationController.java
@@ -17,7 +17,6 @@ import java.util.stream.Collectors;
 
 @RestController
 @ConditionalOnWebApplication
-@CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api/location")
 public class LocationController {
 
--- event-api/src/main/java/learn/events/controllers/ResourcesController.java
+++ event-api/src/main/java/learn/events/controllers/ResourcesController.java
@@ -16,7 +16,6 @@ import java.util.stream.Collectors;
 
 @RestController
 @ConditionalOnWebApplication
-@CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api/resources")
 public class ResourcesController {
 
